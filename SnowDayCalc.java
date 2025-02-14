e9182ae615b4f58b27f0cfc7856b2828b35ce25b
alexblack8-cs2 <ablack27@riverdale.edu>
2025-02-14 11:17:26 -0500

    day one

--- SnowDayCalc.java
+++ SnowDayCalc.java
@@ -1,4 +1,22 @@
 package PACKAGE_NAME;
 
 public class SnowDayCalc {
+    public static void main(String[] args) {
+        int Temp = Integer.valueOf(args[0]);
+        double %chanceOfsnowstorm = Double.valueOf(args[1]);
+        char dayOfTheWeek = S.charAt(args[2]);
+        char typeOfschool = S.charAt(args[3]);
+        boolean scheduledEvents = Boolean.valueOf(args[4]);
+        double chanceOfsnowDay = 0;
+
+        if (temp <= 32) {
+            chanceOfsnowDay += 100 * 0.4;
+        }
+        else {
+            chanceOfsnowday-= 100 * 0.4;
+        }
+
+
+
+    }
 }
